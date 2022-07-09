81e9ea4c1ffe970653009916478ef1c5bf31fb77
Michalis Zamparas <mikezaby@gmail.com>
2022-07-09 23:17:24 +0300

    Move Osc1,2,3 to separated component

---
+++ src/Synth/Oscillators.tsx
@@ -0,0 +1,11 @@
+import Oscillator from "components/audio_modules/Oscillator";
+
+export default function Oscillators() {
+  return (
+    <div>
+      <Oscillator title="Osc 1" />
+      <Oscillator title="Osc 2" />
+      <Oscillator title="Osc 3" />
+    </div>
+  );
+}
--- src/Synth/index.tsx
+++ src/Synth/index.tsx
@@ -2,9 +2,9 @@ import { useEffect, useState } from "react";
 import { Transport } from "tone";
 
 import MidiDeviceSelector from "./components/MidiDeviceSelector";
-import Oscillator from "./components/audio_modules/Oscillator";
 import Envelope from "./components/audio_modules/Envelope";
 
+import Oscillators from "./Oscillators";
 import Mixer from "./Mixer";
 
 export default function Synth() {
@@ -20,9 +20,7 @@ export default function Synth() {
     <>
       {!enabled && <button onClick={() => setEnabled(true)}>Start</button>}
       <MidiDeviceSelector />
-      {enabled && <Oscillator title="Osc 1" />}
-      {enabled && <Oscillator title="Osc 2" />}
-      {enabled && <Oscillator title="Osc 3" />}
+      {enabled && <Oscillators />}
       {enabled && <Mixer />}
       {enabled && <Envelope title="Amp Envelope" amp={true} />}
     </>
