a18d4335e06f5e61daa1d474090c0b2b7ff1a611
Michalis Zamparas <mikezaby@gmail.com>
2023-04-21 20:28:29 +0300

    Enhance Note with extra info

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ export { default as MidiDevice } from "./MidiDevice";
 export { default as MidiDeviceManager } from "./MidiDeviceManager";
 export { default as Note } from "./Note";
 
+export type { INote } from "./Note";
 export type { MidiDeviceInterface } from "./MidiDevice";
 export type { ModuleInterface, AudioModule } from "./Module";
 
