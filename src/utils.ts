2b3579a1426052da9bfa08b28be778a46b60ec37
Michalis Zamparas <mikezaby@gmail.com>
2024-02-18 11:13:23 +0200

    Make determenistic ids look alike a uuid

--- src/utils.ts
+++ src/utils.ts
@@ -5,8 +5,14 @@ export function deterministicId(
   additionalString: string
 ): string {
   const combinedString = `${originalId}:${additionalString}`;
-  const hash = CryptoJS.SHA256(combinedString);
-  const newId = hash.toString(CryptoJS.enc.Hex);
+  const hash = CryptoJS.SHA256(combinedString).toString(CryptoJS.enc.Hex);
+
+  const newId = `${hash.substring(0, 8)}-${hash.substring(
+    8,
+    12
+  )}-4${hash.substring(13, 16)}-${
+    "89AB"[parseInt(hash.substring(17, 18), 16) % 4]
+  }${hash.substring(18, 21)}-${hash.substring(21, 33)}`;
 
   return newId;
 }
